64928da9bbab8263a93b5d1d99a721abe1f5dbe6
Yunus Emre Gülbahar <yunusemregulbahar1@gmail.com>
2025-10-08 00:28:17 +0300

    Player hareket scripti eklendi
    Unity ile geliştirilmiş 2D top-down RPG oyunu. Karakterle dünyayı keşfet, düşmanlarla savaş ve görevleri tamamla.

---
+++ Knight’s Oath/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody2D  rb;
+    public Animator anim;
+    public int facingDirection = 1;
+
+  
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
